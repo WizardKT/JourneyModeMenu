bc6522981a05a009a14f41293075dbc57d7afbc2
WizardKT <deniskovushka335@gmail.com>
2026-05-10 19:24:34 +0700

    - Optimized code structure - Fixed bugs

--- src/main/java/me/wizicl/journeymode/JourneyUtils.java
+++ src/main/java/me/wizicl/journeymode/JourneyUtils.java
@@ -1,13 +1,28 @@
 package me.wizicl.journeymode;
 
+import me.wizicl.journeymode.capabilities.IResearch;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.SoundEvents;
 import net.minecraft.item.ItemStack;
 
 public class JourneyUtils {
 
+    // Сколько надо на изучение предмета
     public static int getRequiredAmount(ItemStack stack) {
+
+        // Если пусто - выводить 0
         if (stack.isEmpty()) return 0;
+
+        // Добавляем переменную максимального стака
         int max = stack.getMaxStackSize();
 
+        // Умножаем стак на коэффициент сложности
         return max * JourneyConfig.stackMult;
+    }
+
+    public static boolean researchFinished(EntityPlayer player, IResearch cap, ItemStack stack) {
+
+        player.playSound(SoundEvents.ENTITY_PLAYER_LEVELUP, 1.0F, 1.0F);
+        return true;
     }
 }
