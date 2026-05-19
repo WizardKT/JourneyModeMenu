df86eee0eb509b5d5e156828ad67d833db638b4c
WizardKT <deniskovushka335@gmail.com>
2026-05-20 02:10:40 +0700

    - QoL

--- src/main/java/me/wizicl/journeymode/JourneyMode.java
+++ src/main/java/me/wizicl/journeymode/JourneyMode.java
@@ -33,12 +33,16 @@ public class JourneyMode {
     @Mod.Instance
     public static JourneyMode instance;
 
+    /// Инициализация переменных для модификации
+
     public static final String MODID = "journeymode";
     public static final String NAME = "Journey Mode";
     public static final String VERSION = "1.6.1";
     public static final String MC_VERSION = "1.12.2";
 
     public static Logger logger;
+
+    /// Создание инстанса для сервера и клиента
 
     @Mod.EventHandler
     public void serverStarting(FMLServerStartingEvent event) {
@@ -52,13 +56,18 @@ public class JourneyMode {
             clientSide = "me.wizicl.journeymode.proxy.ClientProxy",
             serverSide = "me.wizicl.journeymode.proxy.CommonProxy"
     )
-
     public static CommonProxy proxy;
 
-    // Пред-инициализация. Регистрирует блоки, предметы и отправку сообщений в лог.
+    /// ====================================================
+    /// === Предварительная инициализация мода (preInit) ===
+    /// ====================================================
+
+    /** * Предварительная инициализация мода. Здесь мы создаем конфиги и регистрируем все наши блоки, предметы, рецепты и т.д.
+     */
+
     @Mod.EventHandler
     public void preInit(FMLPreInitializationEvent event) {
-        logger = event.getModLog();
+        logger = event.getModLog(); // Создание логера для записи сообщений в консоль.
         logger.info("Создаю конфиги...");
         proxy.preInit(event);
 
@@ -66,7 +75,7 @@ public class JourneyMode {
         CapabilityManager.INSTANCE.register(IResearch.class, new Capability.IStorage<IResearch>() {
             @Override
             public net.minecraft.nbt.NBTBase writeNBT(Capability<IResearch> capability, IResearch instance, EnumFacing side) {
-                return new net.minecraft.nbt.NBTTagCompound(); // Заглушка
+                return new net.minecraft.nbt.NBTTagCompound(); // Возвращаем
             }
 
             @Override
@@ -77,14 +86,26 @@ public class JourneyMode {
 
     }
 
-    // Инициализация. Загрузка рецептов, событий, сущностей.
+    /// ==================================
+    /// === Инициализация мода (init) ===
+    /// ==================================
+
+    /** * Инициализация мода. Здесь мы регистрируем все наши блоки, предметы, рецепты и т.д.
+     */
+
     @Mod.EventHandler
     public void init(FMLInitializationEvent event) {
         proxy.init(event);
         logger.info("Загружаю рецепты и события...");
     }
 
-    // Пост-инициализация. Совместимость с другими модами.
+    /// ==================================
+    /// === Пост-инициализация (post-init) ===
+    /// ==================================
+
+    /** * Пост-инициализация мода. Здесь мы можем выполнить дополнительные действия после того, как все другие моды инициализировались.
+     */
+
     @Mod.EventHandler
     public void postInit(FMLPostInitializationEvent event) {
         logger.info("Оформляю совместимость с другими модами");
